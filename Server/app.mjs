129a5153113d9314111576be055dff4d2159fef4
JoSantana0005 <joseasantana05@gmail.com>
2025-11-12 11:20:22 -0400

    refactor: :boom: Eliminando la estructura abnterior y cambiando la estructura

--- Server/app.mjs
+++ Server/app.mjs
@@ -2,12 +2,6 @@ import express, { json } from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import { CONFIG_SERVER } from './config/config.mjs';
-import { RouteAuth } from './api/RouteAuth.mjs';
-import { RouteCourses } from './api/RouteCourses.mjs';
-import { RouteCategory } from './api/RouteCategory.mjs';
-import { RouteInstructors } from './api/RouteInstructors.mjs';
-import { RouteLessons } from './api/RouteLessons.mjs';
-import { RouteCourseSections } from './api/RouteCourseSections.mjs';
 
 
 // Inicio servidor
@@ -22,13 +16,6 @@ app.use('/uploads/images', express.static('uploads/images'));
 app.use('/uploads/videos', express.static('uploads/videos'));
 app.use('/uploads/lessons', express.static('uploads/lessons'));
 
-// Rutas
-app.use(`${CONFIG_SERVER.basePath}/auth`, RouteAuth);
-app.use(`${CONFIG_SERVER.basePath}/courses`, RouteCourses);
-app.use(`${CONFIG_SERVER.basePath}/categories`, RouteCategory);
-app.use(`${CONFIG_SERVER.basePath}/instructors`, RouteInstructors);
-app.use(`${CONFIG_SERVER.basePath}/lessons`, RouteLessons);
-app.use(`${CONFIG_SERVER.basePath}/sections`, RouteCourseSections);
 
 // Escucho Servidor
 if (CONFIG_SERVER.node !== 'test' || CONFIG_SERVER.node !== 'production') {
